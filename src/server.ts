d314c76097110b0ec20b70bd5b54e146dbf2d908
Diego <diegocamyy@gmailcom>
2021-06-09 14:34:55 -0300

    Added graphql

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,31 @@
-const hi = "hello";
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const PORT = 4000;
+
+const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+app.get("/", (req, res) => {
+  res.json({
+    hello: "world",
+  });
+});
+
+app.listen({ port: PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
